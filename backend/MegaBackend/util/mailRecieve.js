fa525d61616e0d25185ea1c50413e0b0200def85
Aman-Kasaudhan <rajg99842@gmail.com>
2026-01-01 19:13:58 +0530

    Update mailRecieve.js

--- backend/MegaBackend/util/mailRecieve.js
+++ backend/MegaBackend/util/mailRecieve.js
@@ -2,8 +2,58 @@ const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
 
+function generateTicketId() {
+  const prefix = "TCK";
+  const randomPart = Math.random().toString(36).substring(2, 8).toUpperCase();
+  const timestamp = Date.now().toString().slice(-5);
+  return `${prefix}-${randomPart}-${timestamp}`;
+}
+
+
+const ticketId = generateTicketId();
+
+ 
+
 exports.contactAdmin=async (req, res) => {
   const { firstName, lastName, email, phoneCode, phoneNumber, message } = req.body;
+
+
+  const userConfirmationHtml = `
+  <div style="font-family: Arial, sans-serif; background:#f9f9f9; padding:20px;">
+    <div style="max-width:600px; margin:auto; background:#ffffff; padding:20px; border-radius:8px;">
+      
+      <h2 style="color:#2c3e50;">Thank You for Contacting Us</h2>
+      
+      <p>Hi <b>${firstName} ${lastName}</b>,</p>
+      
+      <p>
+        We have successfully received your message.  
+        Our support team will review it and get back to you shortly.
+      </p>
+
+      <hr style="margin:20px 0;" />
+
+      <p><b>🎫 Ticket ID:</b> ${ticketId}</p>
+      <p><b>📧 Email:</b> ${process.env.MAIL_USER}</p>
+      <p><b>📞 Phone:</b> +91 9125275840</p>
+
+       
+
+      <hr style="margin:20px 0;" />
+
+      <p style="font-size:14px; color:#555;">
+        Please keep your <b>Ticket ID</b> for future reference.
+      </p>
+
+      <p style="margin-top:30px;">
+        Best regards,<br/>
+        <b>Support Team</b><br/>
+        StudyDesk
+      </p>
+    </div>
+  </div>
+`;
+
 
   try {
     const transporter = nodemailer.createTransport({
@@ -16,19 +66,15 @@ exports.contactAdmin=async (req, res) => {
 
     await transporter.sendMail({
       from: `"StudyDesk Contact" <${process.env.MAIL_USER}>`,
-      to: process.env.MAIL_USER, // your email
-      subject: "New Contact Form Message",
-      html: `
-        <h3>New Contact Message</h3>
-        <p><b>Name:</b> ${firstName} ${lastName}</p>
-        <p><b>Email:</b> ${email}</p>
-        <p><b>Phone:</b> ${phoneCode} ${phoneNumber}</p>
-        <p><b>Message:</b> ${message}</p>
-      `,
+      to: process.env.MAIL_USER, 
+      to:email,
+      subject: `We received your message | Ticket ID: ${ticketId}`,
+      html: userConfirmationHtml,
     });
 
     res.status(200).json({ success: true });
   } catch (error) {
+    console.error(error.message)
     res.status(500).json({ success: false, message: error.message });
   }
 };
