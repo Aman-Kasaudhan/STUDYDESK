1c32108fffa4caec9f7cc6ea3856820a94a2fefb
Aman-Kasaudhan <rajg99842@gmail.com>
2026-03-16 22:37:47 +0530

    Update navbar.js

--- frontend/src/MegaFrontend/component/admin/navbar.js
+++ frontend/src/MegaFrontend/component/admin/navbar.js
@@ -1,4 +1,4 @@
-import { Link, NavLink } from "react-router-dom";
+import {  NavLink } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { logout } from "../../slice/authSlice";
 import "./navbar1.css";
