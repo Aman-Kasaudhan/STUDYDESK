6ecc11e43d7aba991c3f6a0514705d022b361b19
Aman-Kasaudhan <rajg99842@gmail.com>
2026-06-02 22:36:36 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import rootReducer from '../MegaFrontend/reducer/rootReducer';
 
-// import rootReducer from './MegaFrontend/reducer/rootReducer'
-
-
-
-// import reportWebVitals from '../../reportWebVitals';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {Provider} from 'react-redux'
-// import store from './MegaFrontend/redux/store';
-import Study from './MegaFrontend/Study'
-// import { configureStore } from '@reduxjs/toolkit';
- import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from 'react-redux';
+
+import Study from './MegaFrontend/Study';
+
+import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./MegaFrontend/reducer/store";
-import { logout,setToken } from './MegaFrontend/slice/authSlice';
-import { clearUser,setUser } from './MegaFrontend/slice/profileSlice';
+import { logout, setToken } from './MegaFrontend/slice/authSlice';
+import { clearUser, setUser } from './MegaFrontend/slice/profileSlice';
 import axios from 'axios';
+
+document.title = "Studesk";
 
 const storedUser = localStorage.getItem("user");
 const storedToken = localStorage.getItem("token");
@@ -29,6 +25,7 @@ if (storedUser && storedToken) {
   store.dispatch(setUser(JSON.parse(storedUser)));
   store.dispatch(setToken(storedToken));
 }
+
 axios.interceptors.response.use(
   (res) => res,
   (err) => {
@@ -41,19 +38,18 @@ axios.interceptors.response.use(
     return Promise.reject(err);
   }
 );
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  // <React.StrictMode>
- <Provider store={store}>
+  <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
         <Study />
         <ToastContainer />
       </BrowserRouter>
     </PersistGate>
-      </Provider>
-      // {/* </React.StrictMode> */}
+  </Provider>
 );
 
 reportWebVitals();
